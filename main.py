a9cf29355595a3f6ed73ebfffdcefbefb040ac6d
Germain Hirwa <higermain1@gmail.com>
2024-07-17 11:17:22 +0200

    initialized the scree Camera

--- main.py
+++ main.py
@@ -4,3 +4,10 @@ from cvzone.HandTrackingModule import HandDetector
 import time
 import random
 import mediapipe
+
+cap = cv2.VideoCapture(0)  # This is initializing the camera
+cap.set(3, 640)  # Here we are setting a fixed size (width to 640) in every case be it with a pc or webcam
+cap.set(3, 480)  # Setting a fixed height
+
+detector = HandDetector(maxHands=1)  # This helps detect the hand
+
